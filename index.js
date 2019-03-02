ee3838a11d800f51f971730dd5696fbf78eec136
Anubhav311 <anubhavbhambri1@gmail.com>
2019-03-02 20:17:26 +0530

    Linked index.html with index.js and add answer testing feature.

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("I am a mad man");
+
+const input = [1, 2, 3];
+const algorithms = [1, 2, 3];
+
+let varb;
+function saveValue() {
+    varb = document.getElementById("output").value;
+    if(varb == 6) {
+        alert("Your answer is right");
+    } else {
+        alert("Your answer is wrong");
+    }
+}
+
